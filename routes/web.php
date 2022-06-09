a93b01d9af1c2a95c13eedb004a83968777acab1
Ihsan Bagus <ihsan.bagus@iforce.co.id>
2022-06-09 15:46:35 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group(['prefix' => 'api/auth'], function () use ($router) {
+    $router->post('login', ['uses' => 'AuthController@login']);
+    $router->post('logout', ['uses' => 'AuthController@logout']);
+    $router->post('refresh', ['uses' => 'AuthController@refresh']);
+    $router->post('me', ['uses' => 'AuthController@me']);
+});
